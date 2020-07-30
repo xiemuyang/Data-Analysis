cc38164798cb0dbe9648f04454fd28809ddd9887
Xie Muyang <67192511+xiemuyang@users.noreply.github.com>
2020-07-30 10:12:49 +0800

    Add files via upload

--- ProjectB/Apriori_Order.py
+++ ProjectB/Apriori_Order.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 使用Apriori算法，挖掘订单中的频繁项集及关联规则
-
 """
 
 import pandas as pd
@@ -10,7 +9,7 @@ import time
 # 数据加载
 data = pd.read_csv('./订单表.csv',encoding='gbk')
 # 筛选所需字段
-train=data[['客户ID','订单日期','产品ID']]
+train=data[['客户ID','产品名称']]
 #print(train.head())
 
 # 采用efficient_apriori工具包
@@ -19,10 +18,10 @@ def rule1():
     start = time.time()
     transactions= []
     # 基于客户ID和订单日期字段汇总订单item
-    for name, group in train.groupby(['客户ID','订单日期']):
-        temp_set=set(group['产品ID'])
+    for name, group in train.groupby(['客户ID']):
+        temp_set=set(group['产品名称'])
         transactions.append(temp_set)
-    print(transactions[:10])
+    #print(transactions[:10])
     # 挖掘频繁项集和频繁规则
     itemsets, rules = apriori(transactions, min_support=0.02,  min_confidence=0.5)
     print('频繁项集：', itemsets)
@@ -41,11 +40,14 @@ def rule2():
     from mlxtend.frequent_patterns import association_rules
     pd.options.display.max_columns=100
     start = time.time()
-    temp_df=train.groupby(['客户ID','订单日期','产品ID'])['产品ID'].count().unstack().reset_index().fillna(0)
+    #对数据进行独热编码
+    temp_df=train.groupby(['客户ID','产品名称'])['产品名称'].count().unstack().reset_index().fillna(0)
     temp_df['ID']=range(len(temp_df))
-    hot_encoded_df=temp_df.drop(['客户ID','订单日期'],1).set_index('ID')
+    hot_encoded_df=temp_df.drop(['客户ID'],1).set_index('ID')
     #print(hot_encoded_df.head())
+    #运用函数对数据进行0/1化处理
     hot_encoded_df=hot_encoded_df.applymap(encode_units)
+    #挖掘频繁项集和频繁规则
     frequent_itemsets = apriori(hot_encoded_df, min_support=0.02, use_colnames=True)
     rules = association_rules(frequent_itemsets, metric="lift", min_threshold=0.5)
     print("频繁项集：", frequent_itemsets)
